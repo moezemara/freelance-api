eafa7311fedbe8d20117dbfad519db0e4eb915e7
Mohamed Ezzat <moezemara@gmail.com>
2022-05-14 04:18:20 +0000

    added todo routes

--- v1/freelancer/freelancer.router.js
+++ v1/freelancer/freelancer.router.js
@@ -13,4 +13,36 @@ router.post("/profile/:profile_id/update", auth.basic, auth.freelancer,
 schemaChecker.checkparams(schema.updateprofile_params_schema), schemaChecker.checkbody(schema.updateprofile_body_schema), 
 updateprofile)
 
+router.post("/profile/:profile_id/activate") // activate profile to be used getting jobs
+router.get("/job") // get all jobs matching active profile skills
+router.get("/job/:job_id") // get details of a job
+
+router.post("/proposal") // post proposal to a job
+router.get("/proposal") // get all applied proposals
+router.get("/proposal/active") // get all proposal which still has job active
+router.get("/proposal/archived") // get all proposal which has contract or inactive job or canceld ones
+
+router.post("/proposal/:proposal_id/cancel") // cancels active proposal
+router.post("/proposal/:proposal_id/boost") // boosts active proposal (adds badge on it which costs connects)
+
+router.get("/contract") // get all user contracts
+router.get("/contract/:contract_id") // get contract data
+router.get("/contract/pending") // get all contracts not started yet (on interview)
+router.get("/contract/active") // get all ongoing contracts (not yet finished)
+router.get("/contract/archived") // get all finished contracts or unaccepted ones
+
+router.post("/contract/pending/:contract_id/updatestatus") // accept, cancel, end offer
+router.post("/contract/pending/:contract_id/updateprice") // update interview contract price (only when not pending acceptance)
+
+router.get("/contract/:contract_id/milestone") // get milestones of a contract
+router.get("/contract/:contract_id/milestone/:milestone_id/add") // add milestone only to (pending and active contracts)
+router.get("/contract/:contract_id/milestone/:milestone_id/remove") // remove milestone only from (pending and active contracts)
+router.get("/contract/:contract_id/milestone/:milestone_id/update") // update milestone only on (pending and active contracts)
+
+router.post("/contract/:contract_id/end") // end contract only when active
+
+// feedbacks only available when contract ends
+router.post("/contract/:contract_id/feedback/add") // add feedback to contract
+router.post("/contract/:contract_id/feedback/update") // update feedback of contract
+
 export default router;
--- v1/user/user.router.js
+++ v1/user/user.router.js
@@ -9,4 +9,12 @@ router.post("/login", schemaChecker.checkbody(schema.login_schema), login)
 router.post("/logout", logout)
 router.post("/signup", schemaChecker.checkbody(schema.signup_schema), signup)
 
+router.get("/account/profile") // get account details
+router.post("/account/update/password") // change account password
+router.post("/account/update/name") // change first and last name
+router.post("/account/update/email") // change email
+router.post("/account/update/phone") // change phone number
+router.post("/account/update/address") // change address and country
+router.post("/account/update/profile") // change profile picture
+
 export default router;
