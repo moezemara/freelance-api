f778086e82168b95abb81e0b45bf736e4f2460fc
Mohamed Ezzat <moezemara@gmail.com>
2022-05-21 18:50:08 +0000

    changed authentication for the applied proposals route

--- v1/proposal/proposal.router.js
+++ v1/proposal/proposal.router.js
@@ -8,7 +8,7 @@ const router = express.Router()
 
 router.post("/apply/:job_id", auth.basic, auth.freelancer, schemaChecker.checkbody(schema.applytojob_schema), applytojob) // apply to a job
 router.get("/get/pending", auth.basic, auth.freelancer, getpendingproposals) // get all applied proposals
-router.get("/get/proposals/:job_id", auth.basic, auth.freelancer, getproposalsofjob) // get pending applied proposals of certain job
+router.get("/get/proposals/:job_id", auth.basic, auth.client, getproposalsofjob) // get pending applied proposals of certain job
 
 router.post("/:proposal_id/cancel") // cancels active proposal
 router.post("/:proposal_id/boost") // boosts active proposal (adds badge on it which costs connects)
